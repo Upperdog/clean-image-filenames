41f372fd493fb3b24caf6699b7ccd347ef13f529
Emil Gustafsson <emil@upperdog.se>
2014-09-28 23:12:23 +0200

    Initial commit. Plugin version 1.0.

---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== Sanitize Image Filenames ===
+Contributors: Upperdog, Gesen
+Tags: upload, images, files, media, 
+Requires at least: 3.0
+Tested up to: 4.0
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Say goodbye to bad image filenames like Château-de-Ferrières.jpg and say hello to nice and clean filenames like chateau-de-ferrieres.jpg. This plugins sanitizes filenames for select mime types when the file is being uploaded to the WordPress media library. The plugin can be used on single sites or multisite networks.
+
+== Description ==
+
+Say goodbye to bad image filenames like Château-de-Ferrières.jpg and say hello to nice and clean filenames like chateau-de-ferrieres.jpg. This plugins sanitizes filenames for select mime types when the file is being uploaded to the WordPress media library. The plugin can be used on single sites or multisite networks.
+
+= Features = 
+
+* Add your own mime types
+* Multisite compatible
+* Works with custom upload_dir setups
+* Doesn't alter your database or uploads settings
+
+= Add your own mime types =
+
+You can easily add you own mime types by using the sif_valid_mime_types filter in your theme or plugin. Example usage: 
+
+`function custom_sif_valid_mime_types() {`
+``
+`	$valid_mime_types = array(`
+`		'application/pdf', `
+`		'image/jpeg', `
+`		'image/png', `
+`	);`
+``
+`	return $valid_mime_types;`
+`}`
+`add_filter('sif_valid_mime_types', 'custom_sif_valid_mime_types');`
+
+== Installation ==
+
+1. Upload the `sanitize-image-filenames` directory to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release.
---
+++ sanitize-image-filenames.php
@@ -0,0 +1,73 @@
+<?php 
+/**
+ * Plugin Name: Sanitize Image Filenames
+ * Description: Say goodbye to bad image filenames like Château-de-Ferrières.jpg and say hello to nice and clean filenames like chateau-de-ferrieres.jpg. This plugins sanitizes filenames for select mime types when the file is being uploaded to the WordPress media library. The plugin can be used on single sites or multisite networks.
+ * Version: 1.0
+ * Author: UPPERDOG
+ * Author URI: http://upperdog.com
+ * Author Email: hello@upperdog.se
+ * License: GPL2
+ */
+
+/*  Copyright 2014 Upperdog (email : hello@upperdog.se)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if(!defined('ABSPATH')) {
+	exit;
+}
+
+class SanitizeImageFilenames {
+
+	function __construct() {
+		add_action('wp_handle_upload_prefilter', array($this, 'upload_filter'));
+	}
+
+	function upload_filter($file) {
+
+		/**
+		 * Only sanitize the filename for files that has one of these mime types. 
+		 * The list of mime types can be customized using the sif_valid_mime_types filter.
+		 * For a complete list of mime types, see http://en.wikipedia.org/wiki/Internet_media_type
+		 */
+		$valid_mime_types = array(
+			'image/bmp', 
+			'image/gif', 
+			'image/jpeg', 
+			'image/pjpeg', 
+			'image/png', 
+		);
+
+
+		/**
+		 * If the current file's mime type is in the array of valid mime types, the filename 
+		 * will be sanitized and saved to the original $file array.
+		 */
+		if (in_array($file['type'], apply_filters('sif_valid_mime_types', $valid_mime_types))) {
+			$path = pathinfo($file['name']);
+			$new_filename = preg_replace('/.' . $path['extension'] . '$/', '', $file['name']);
+			$file['name'] = sanitize_title($new_filename) . '.' . $path['extension'];
+		}
+
+
+		/**
+		 * Return the $file array. If the filename wasn't changed, the input array 
+		 * is returned without any modifications.
+		 */
+	    return $file;
+	}
+}
+
+$sanitize_image_filenames = new SanitizeImageFilenames();
