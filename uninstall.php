83998e2e4cb775335402d8b99bcfc20471ef8219
Emil Gustafsson <emil@upperdog.com>
2020-10-11 21:27:02 +0200

    Rewrite filename cleaning function. Make sure code is compliant with WordPress Coding Standards. Update documentation.

--- uninstall.php
+++ uninstall.php
@@ -1,17 +1,19 @@
 <?php
 /**
- * Uninstall plugin
- * 
- * This file is executed when the plugin is uninstalled and deletes plugin settings. 
+ * Uninstall plugin.
+ *
+ * This file is executed when the plugin is uninstalled and deletes plugin settings.
+ *
+ * @package clean-image-filenames
  *
  * @since 1.1.1
  */
 
-// Exit if uninstall is not called from WordPress
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
+// Exit if uninstall is not called from WordPress.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
+}
 
-// Delete plugin settings
+// Delete plugin settings.
 delete_option( 'clean_image_filenames_plugin_version' );
 delete_option( 'clean_image_filenames_mime_types' );
-?>
